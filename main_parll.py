8b4baa6e04bea93441ade4a26e41329334e5f2ca
Aidan Gray <aidancgray@gmail.com>
2023-10-13 20:21:41 +0000

    created parll versions of primary files

---
+++ main_parll.py
@@ -0,0 +1,180 @@
+#!/usr/bin/python3.6
+# main.py
+# 04/04/2023
+# Aidan Gray
+# aidan.gray@idg.jhu.edu
+#
+# Main script for running the First Order Data Out (FODO) 
+# for JHU's Rocket Lab.
+###############################################################################
+
+import sys
+import os
+import asyncio
+import netifaces
+import logging
+import argparse
+import configparser
+import shlex
+import wiringpi as gpio
+
+from change_MAC_udp import changeMAC_UDPServer
+from udp_server_async_parll import AsyncUDPServer
+from shift_register import GPIO_to_cRIO
+#from packet_analyzer import packetAnalyzer
+from packet_handler_parll import packetHandler
+
+#       IDX |  0    1    2    3    4    5    6    7    8    9   10   11   12
+#      PHYS |  6    7    8   15   16   29   30   31   32   33   35   37   39
+#PIN_LIST = [470, 471, 472, 506, 505, 423, 422, 425, 424, 507, 477, 421, 462]
+PIN_LIST  = [  1,   2,   3,   5,   6,  13,  14,  15,  16,  17,  18,  19,  20]
+
+SHIFTREG_INPUT_PIN = PIN_LIST[0]
+SHIFTREG_CLOCK_PIN = PIN_LIST[1]
+SHIFTREG_LATCH_PIN = PIN_LIST[2]
+SHIFTREG_CLEAR_PIN = PIN_LIST[3]
+SHIFTREG_OUTEN_PIN = PIN_LIST[4]
+
+SNAP_FIFO_FULL_PIN = PIN_LIST[5]
+SNAP_FIFO_EMPTY_PIN = PIN_LIST[6]
+SNAP_FIFO_READ_PIN = PIN_LIST[7]
+
+DELAY = 2000
+IP_ANN_DELAY = 10 #seconds
+
+IDG_CONFIG = {
+    "SOURCE_IP" : "172.16.1.112",
+    "SOURCE_PORT" : 1025,
+    "DESTINATION_IP" : "172.16.1.125",
+    "DESTINATION_PORT" : 60000,
+    "NETWORK_LAYER" : "udp"}
+
+ROCKET_CONFIG = {
+    "SOURCE_IP" : "192.168.1.10",
+    "SOURCE_PORT" : 62510,
+    "DESTINATION_IP" : "192.168.1.100",
+    "DESTINATION_PORT" : 60000,
+    "NETWORK_LAYER" : "udp"}
+
+def custom_except_hook(loop, context):
+    logger = logging.getLogger('fodo')
+    logger.setLevel(logging.WARN)
+    
+    if repr(context['exception']) == 'SystemExit()':
+        logger.debug('Exiting Program...')
+
+async def runFODO(loop, opts):
+    logging.basicConfig(datefmt = "%Y-%m-%d %H:%M:%S",
+                        format = '%(asctime)s.%(msecs)03dZ ' \
+                                '%(name)-10s %(levelno)s ' \
+                                '%(filename)s:%(lineno)d %(message)s')
+    
+    logger = logging.getLogger('fodo')
+    logger.setLevel(opts.logLevel)
+    logger.debug('~~~~~~starting log~~~~~~')
+    
+    if opts.rocketConfig:
+        config = ROCKET_CONFIG
+    else:
+        config = IDG_CONFIG
+    
+    srcIP = config["SOURCE_IP"]
+    srcPort = config["SOURCE_PORT"]
+    dstIP = config["DESTINATION_IP"]
+    dstPort = config["DESTINATION_PORT"]
+    layer = config["NETWORK_LAYER"] 
+
+    try:
+        dstIP = netifaces.ifaddresses('eth0')[netifaces.AF_INET][0]['addr']
+    except:
+        pass
+
+    # For scapy.sniff(). Unused in newest configuration (05/22/2023)
+    # bpf_filter = f"src host {srcIP} and " \
+    #             f"src port {srcPort} and " \
+    #             f"dst host {dstIP} and " \
+    #             f"dst port {dstPort} and " \
+    #             f"{layer}"
+
+    # Asynchronous Server to allow the user to change/spoof the MAC Address
+    changeMAC = changeMAC_UDPServer(loop, 
+                                    hostname="0.0.0.0", 
+                                    port=61000, 
+                                    password="3701SanMartin")
+
+    udpServer = AsyncUDPServer(loop, dstIP, dstPort)
+    
+    pktHandler = packetHandler(qPacket=udpServer.qPacket,
+                               qXmit=udpServer.qXmit)
+    
+    shiftReg = GPIO_to_cRIO(qXmit=udpServer.qXmit,
+                            inputPin=SHIFTREG_INPUT_PIN,
+                            clockPin=SHIFTREG_CLOCK_PIN,
+                            latchPin=SHIFTREG_LATCH_PIN,
+                            clearPin=SHIFTREG_CLEAR_PIN,
+                            outEnPin=SHIFTREG_OUTEN_PIN,
+                            snapFullPin=SNAP_FIFO_FULL_PIN, 
+                            snapEmptyPin=SNAP_FIFO_EMPTY_PIN, 
+                            snapReadPin=SNAP_FIFO_READ_PIN,
+                            order=gpio.MSBFIRST,
+                            clockTime=opts.tickRate)
+
+    ipAnnTask = loop.create_task(ipAnnounce)
+    
+    await asyncio.gather(changeMAC.start_server(), 
+                         udpServer.start_server(), 
+                         pktHandler.start(),
+                         shiftReg.start(),
+                         ipAnnounce()
+                         )
+    
+
+async def ipAnnounce():
+    while True:
+        #os.system("arping -A -c 1 -I eth0 192.168.1.100")
+        os.system("ping -c 1 192.168.1.10")
+        await asyncio.sleep(IP_ANN_DELAY)
+    
+def parseConfigFile(filename):
+    config = configparser.ConfigParser()
+    config.read(filename)
+    filterInfo = {}
+    filterInfo['SOURCE_IP'] = config['FILTER INFO']['SOURCE IP']
+    filterInfo['SOURCE_PORT'] = config['FILTER INFO']['SOURCE PORT']
+    filterInfo['DESTINATION_IP'] = config['FILTER INFO']['DESTINATION IP']
+    filterInfo['DESTINATION_PORT'] = config['FILTER INFO']['DESTINATION PORT']
+    filterInfo['NETWORK_LAYER'] = config['FILTER INFO']['NETWORK LAYER']
+
+    return filterInfo
+    
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv[1:]
+    if isinstance(argv, str):
+        argv = shlex.split(argv)
+
+    parser = argparse.ArgumentParser(sys.argv[0])
+    parser.add_argument('--rocketConfig', type=bool, default=False,
+                        help='network configuration: True = Rocket | False = IDG Network')
+    parser.add_argument('--logLevel', type=int, default=logging.INFO,
+                        help='logging threshold. 10=debug, 20=info, 30=warn')
+    parser.add_argument('--delay', type=int, default=2000,
+                        help='in milliseconds - used for pausing')
+    parser.add_argument('--tickRate', type=int, default=0,
+                        help='in milliseconds - clock tick rate')
+
+    opts = parser.parse_args(argv)
+    loop = asyncio.get_event_loop()
+    loop.set_exception_handler(custom_except_hook)
+    loop.run_until_complete(runFODO(loop, opts))
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        print('Exiting Program...')
+    finally:
+        loop.close()
+        asyncio.set_event_loop(None)
+    
+if __name__ == "__main__":
+    main()
+    
---
+++ packet_handler_parll.py
@@ -0,0 +1,111 @@
+#!/usr/bin/python3
+# packet_handler.py
+# 04/11/2023
+# Aidan Gray
+# aidan.gray@idg.jhu.edu
+#
+# Packet Handler class for JHU's Rocket Lab.
+###############################################################################
+
+import logging
+import asyncio
+
+SLEEP_TIME = 0.000001
+
+class packetHandler:
+    def __init__(self, qPacket, qXmit):
+        self.logger = logging.getLogger('fodo')
+        self.qPacket = qPacket
+        self.qXmit = qXmit
+        self.packetCount = 0
+
+    async def start(self):
+        while True:
+            if not self.qPacket.empty():
+                pkt = await self.qPacket.get()
+                retData = await self.handlePacket(pkt)
+                if retData != None:
+                    await self.enqueue_xmit(retData)
+            await asyncio.sleep(0.000001)
+
+    async def handlePacket(self, pkt):
+        try:
+            self.logger.debug(f'PACKET_SIZE: {len(pkt)}')
+            if len(pkt) <= 62:
+                self.logger.debug(f'PACKET: {pkt}')
+            else:
+                self.logger.debug(f'PACKET: {pkt[:62]} ... ')
+
+            pktCount = (pkt[3]<<8) + pkt[2]
+
+            # Make sure the data is aligned properly and is newest packet            
+            if (pkt[4] == 0) and pkt[5] == 0 and (pktCount > self.packetCount):
+                numPhotons = (pkt[1]<<8) + pkt[0]
+                numPhotonBytes = numPhotons * 6
+                self.logger.debug(f'NUM_PHOTONS: {numPhotons} ({numPhotonBytes}B)')
+
+                if numPhotons > 0:
+                    # Split the packet into photons (X, Y, P) each consisting of 2 bytes
+                    pktData = pkt[6:]
+                    photonList = [pktData[i:i+6] for i in range(0, numPhotonBytes, 6)]
+                
+                    if len(photonList) <= 10:
+                        self.logger.debug(f'PHOTON_LIST: {photonList}')
+                    else:
+                        self.logger.debug(f'PHOTON_LIST: {photonList[:10]} ... ')
+
+                    photonQueue = []
+                    pNum = 0
+                    for photon in photonList:
+                        self.logger.debug(f'PHOTON[{pNum}]: {photon}')
+                        
+                        # self.logger.debug(f'xA = {photon[1]} << 3')
+                        # self.logger.debug(f'xB = {photon[0]} >> 5')
+                        xA = photon[1]<<3
+                        xB = photon[0]>>5
+                        yA = photon[3]<<3
+                        yB = photon[2]>>5
+
+                        x = (xA + xB) & 255
+                        y = (yA + yB) & 255
+                        # p = photon[4]
+                        
+                        # self.logger.debug(f'x={xA}+{xB}')
+                        # self.logger.debug(f'y={yA}+{yB}')
+                        
+                        photonQueue.append((x, y))
+                        pNum+=1            
+                    
+                    return photonQueue
+                else:
+                    self.logger.debug(f'NO PHOTONS')
+                    await asyncio.sleep(SLEEP_TIME)
+                    return None
+        except:
+            pass
+
+    async def enqueue_xmit(self, data):
+        if self.qXmit.full():
+            self.logger.warn(f'Transmit Data Queue is FULL')
+        else:
+            await self.qXmit.put(data)
+
+async def runPktHandlerTest(loop):
+    pktHandler = packetHandler(qPacket=asyncio.Queue(),
+                               qXmit=asyncio.Queue()
+                               )
+    await asyncio.gather(pktHandler.start())
+
+if __name__ == "__main__":
+    LOG_FORMAT = '%(asctime)s.%(msecs)03dZ %(name)-10s %(levelno)s \
+        %(filename)s:%(lineno)d %(message)s'
+    logging.basicConfig(datefmt = "%Y-%m-%d %H:%M:%S", format = LOG_FORMAT)
+    logger = logging.getLogger('fodo')
+    logger.setLevel(logging.DEBUG)
+    logger.debug('~~~~~~starting log~~~~~~')
+
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(runPktHandlerTest(loop))
+    except KeyboardInterrupt:
+        print('Exiting Program...')
